762a6c17fd54d76a1014d5b40781314682a69303
NBCteam <nbcteam0x@gmail.com>
2020-03-11 20:16:20 +0100

    bootstrap

--- functions.php
+++ functions.php
@@ -5,7 +5,12 @@ function word_work(){
     wp_enqueue_script('js',get_theme_file_uri('js/js.js',NULL,'3.1.1',true));
     wp_enqueue_style('style_css',get_stylesheet_uri());
 }
+
+wp_enqueue_style('bootstrap', get_stylesheet_directory_uri().'/bootstrap/css/bootstrap.min.css');
+wp_enqueue_script('bootstrap', get_stylesheet_directory_uri().'/bootstrap/css/bootstrap.min.css');
+ 
 add_action('wp_enqueue_scripts','word_work');
+
 ?>
  
 
