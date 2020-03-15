4475d4fb0466970e110159afcda8fbc3c73d24e2
NBCteam <nbcteam0x@gmail.com>
2020-03-16 00:20:21 +0100

    upd

--- header.php
+++ header.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en" >
 <head>
+  
   <title> ESTsb </title>
-
+  <link rel="icon" type="image/png" href="img/estlogo.png" />
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>EST Sidi Bennour</title>  
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
   <link rel="stylesheet" href="<?php  echo get_template_directory_uri().'/style.css'?>"> 
   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-  <link rel="icon" href="../EST_SidiBennour.ico" />
+  
+
 </head>
+
 
 <body>
 <div id='cssmenu'>
