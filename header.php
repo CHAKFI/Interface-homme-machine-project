91bde5fd275c58fb3be420d18e0aed93c69f7c8f
NBCteam <nbcteam0x@gmail.com>
2020-03-16 15:42:48 +0100

    upd

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   
   <title> ESTsb </title>
-  <link rel="icon" type="image/png" href="estlogo.png" />
+  <link rel="icon" href="<?php bloginfo('template_directory'); ?>/img/estlogo.png" />
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,7 +18,7 @@
 
 <body>
 
-<a href="index.php"><img src="estsb.png"></a>
+<a href="index.php"></a>
 
 <div id='cssmenu'>
 <ul class="logo-navbar">
