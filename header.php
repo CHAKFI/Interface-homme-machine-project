505f743c72ee4d396bd9f16e3ce04500bb7d3c76
CHAKFI <ahmedchakfi1999@gmail.com>
2020-03-16 20:26:38 +0100

    upd

--- header.php
+++ header.php
@@ -7,17 +7,17 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>EST Sidi Bennour</title>  
-  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
-  <link rel="stylesheet" href="<?php  echo get_template_directory_uri().'/style.css'?>"> 
+  <link rel="stylesheet" href="<?php  echo get_template_directory_uri().'/style.css'?>">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'> 
   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
   
-
 </head>
 
 <body>
 
-<a href="index.php"><img  src="<?php bloginfo('template_directory'); ?>/img/estsb1.png""></img></a>
+
+  <a align='right' href="index.php"><img  src="<?php bloginfo('template_directory'); ?>/img/estsb1.png""></img></a>
+  <img  src="<?php bloginfo('template_directory'); ?>/img/ucd.png""></img>
 
 <div id='cssmenu'>
 <ul class="logo-navbar">
