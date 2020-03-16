646dd240793ec7b3ffb9223f1cb64b7e45476c9f
CHAKFI <ahmedchakfi1999@gmail.com>
2020-03-16 20:44:44 +0100

    styles

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="<?php  echo get_template_directory_uri().'/style.css'?>">
+  <link rel="stylesheet" href="<?php  echo get_template_directory_uri().'/STL.css'?>">
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'> 
   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
   
