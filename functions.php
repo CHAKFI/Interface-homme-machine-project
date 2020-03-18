fea0c94da310003689c034e5214f643a673951f8
CHAKFI <ahmedchakfi1999@gmail.com>
2020-03-18 12:02:07 +0100

    upd

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ function word_work(){
     wp_enqueue_script('jquery',get_theme_file_uri('js/jquery-3.4.1.min.js',NULL,'3.1.1',true));
 }
 
-
+wp_enqueue_script('bootstrap', get_stylesheet_directory_uri().'/js/bootstrap.min.css');
 wp_enqueue_script('bootstrap', get_stylesheet_directory_uri().'/js/bootstrap.min.js');
  
 add_action('wp_enqueue_scripts','word_work');
